90bf2fa96e83fa644ee515bf101ddd7331746de2
DDEC2610 <132554986+DDEC2610@users.noreply.github.com>
2023-05-09 15:33:37 +0200

    Add files via upload

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,13 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+let eingabe1 = prompt("Erste zahl eingeben: "); 
+let eingabe2 = prompt("Zweite Zahl eingeben: ");
+let eingabe3 = prompt("Dritte Zahl eingeben: "); 
+let zahl1 = parseInt(eingabe1);
+let zahl2 = parseInt(eingabe2);
+let zahl3 = parseInt(eingabe2);
+
+summe = zahl1 + zahl2 + zahl3;
+
+console.log (" Die Summe der Zahlen ist: " + summe);
